b3b506d598364443b51b74fd3c77b3e59122410f
John Cline <clinejj@gmail.com>
2016-06-21 23:32:46 -0400

    remove todo

--- src/main/java/com/maple/patternmatcher/Matcher.java
+++ src/main/java/com/maple/patternmatcher/Matcher.java
@@ -24,7 +24,6 @@ public class Matcher {
 	private static Map<String, Field> patternMap = new HashMap<String, Field>();
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		if (args.length < 2) {
 			System.err.println("Must send two arguments, the input file name and output file name");
 			return;
